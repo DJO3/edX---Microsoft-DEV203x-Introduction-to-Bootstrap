0773c037a028012a681f7cc4a6bb0aff25339756
DJO3 <dev@pytools.org>
2015-04-27 17:49:11 -0400

    final

--- lab3/urls.py
+++ lab3/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
     '',
     url(r'^modal/$', views.modal, name='modal'),
+    url(r'^final/$', views.final, name='final'),
 )
--- lab3/views.py
+++ lab3/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def modal(request):
     context_dict = {}
     return render(request, 'lab3/modal.html', context_dict)
+
+
+def final(request):
+    context_dict = {}
+    return render(request, 'lab3/final.html', context_dict)
